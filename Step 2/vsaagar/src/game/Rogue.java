7d588eae7d347fb46ce080b382694e4a79ccab2b
vishrantsaagar <54513607+vishrantsaagar@users.noreply.github.com>
2020-10-27 13:51:46 -0500

    minor

--- Step 2/vsaagar/src/game/Rogue.java
+++ Step 2/vsaagar/src/game/Rogue.java
@@ -11,30 +11,31 @@ import org.xml.sax.SAXException;
 
 public class Rogue implements Runnable{
 
+    public static final int FRAMESPERSECOND = 60;
+    public static final int TIMEPERLOOP = 1000000000 / FRAMESPERSECOND;
     private static ObjectDisplayGrid displayGrid = null;
+    private Thread keyStrokePrinter;
+    private static final int WIDTH = 80;
+    private static final int HEIGHT = 40;
+    public static Dungeon dungeon;
+    public static DungeonXMLHandler handler;
+    public char ch;
 
     public Rogue(int width, int height) {
-    //    displayGrid = new ObjectDisplayGrid(width, height);
+        displayGrid = new ObjectDisplayGrid(width, height);
     }
 
     @Override
     public void run() {
-  //      displayGrid.fireUp();
-  //      for (int step = 1; step < WIDTH / 2; step *= 2) {
-  //          for (int i = 0; i < WIDTH; i += step) {
-  //              for (int j = 0; j < HEIGHT; j += step) {
-  //                  displayGrid.addObjectToDisplay(new Char('X'), i, j);
-  //              }
-  //          }
-
-  //          try {
-  //              Thread.sleep(2000);
-  //          } catch (InterruptedException e) {
-  //              e.printStackTrace(System.err);
-  //          }
-  //          displayGrid.initializeDisplay();
-  //      }
-  }  
+        displayGrid.fireUp();
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace(System.err);
+            }
+            displayGrid.initializeDisplay();
+        }
+    
     public static void main(String[] args) {
         
         // check if a filename is passed in.  If not, print a usage message.
