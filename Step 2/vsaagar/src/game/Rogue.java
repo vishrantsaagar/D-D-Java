14defbe6f7053f0763b856192ad889dfae0ff533
vishrantsaagar <54513607+vishrantsaagar@users.noreply.github.com>
2020-11-01 22:50:48 -0600

    changed run method: java game.Rogue <filename>

--- Step 2/vsaagar/src/game/Rogue.java
+++ Step 2/vsaagar/src/game/Rogue.java
@@ -256,7 +256,7 @@ public class Rogue implements Runnable {
         case 1:
            // note that the relative file path may depend on what IDE you are
        // using.  This worked for NetBeans.
-           fileName = "xmlfiles/" + args[0]; //../xmlfiles. + args[0]
+           fileName = "../xmlfiles/" + args[0]; //../xmlfiles. + args[0]
            break;
         default:
            System.out.println("java Test <xmlfilename>");
